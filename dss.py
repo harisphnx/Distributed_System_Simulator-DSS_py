30515da84b0aaadf0579e23c387a915c024237ec
student <haris.phnx@gmail.com>
2016-02-05 15:55:58 +0530

    removed bug --> machine count not incrementing

--- dss.py
+++ dss.py
@@ -12,11 +12,11 @@ class machine():
     
     q = [multiprocessing.Queue() for i in range(max_instances + 1)]
     # q[0] is unused
-    count = 1
+    count = multiprocessing.Value('i', 1)
 
     def __init__(self):
-        self.mac_id = machine.count
-        machine.count += 1
+        self.mac_id = machine.count.value
+        machine.count.value += 1
 
     def execute_func(self, func_name, *args):
         comm_str = str(func_name) + ' = multiprocessing.Process(name = "' + str(func_name) + '", target = ' + str(func_name) + ', args = ('
@@ -34,12 +34,13 @@ class machine():
         # start the new process
         comm_str = str(func_name) + '.start()'
         exec(comm_str)
+        
 
     def send(self, destination_id, message):
         # send message to the machine with machine_id destination_id
 
         mac_id = int(destination_id[8:])
-        if(mac_id >= machine.count or mac_id <= 0):
+        if(mac_id >= machine.count.value or mac_id <= 0):
             return -1
 
         # message is of the format "hello|2". Meaning message is "hello" from machine with id 2
@@ -51,7 +52,7 @@ class machine():
 
     def recv(self):
         mac_id = self.get_id()
-        if(mac_id >= machine.count or mac_id <= 0):
+        if(mac_id >= machine.count.value or mac_id <= 0):
             return -1, -1
 
         message =  machine.q[mac_id].get().split('|')
